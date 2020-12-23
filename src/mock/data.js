d5938d592633093a3aa95f760905c1a42f59c83e
Stefanos <stefanosu422@gmail.com>
2020-12-22 23:04:22 -0500

    changed header

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Stefanos',
-  subtitle: 'I am a Software engineer', 
+  subtitle: "I\'m a Software engineer", 
   cta: '',
 };
 
